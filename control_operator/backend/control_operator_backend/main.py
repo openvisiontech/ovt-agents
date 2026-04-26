2ff8f1616d0b1db90dc2da184bc96e17cee5b83a
gen.lin <gen@openvisiontech.com>
2026-04-25 23:23:07 -0400

    add license statement

--- control_operator/backend/control_operator_backend/main.py
+++ control_operator/backend/control_operator_backend/main.py
@@ -1,3 +1,14 @@
+'''
+Copyright (c) 2026 by Open Vision Technology, LLC., Massachusetts.
+All rights reserved.
+
+Open Vision Technology, LLC. and its licensors retain all intellectual property
+and proprietary rights in and to this software, related documentation
+and any modifications thereto. Any use, reproduction, disclosure or
+distribution of this software and related documentation without an express
+license agreement from Open Vision Technology, LLC. is strictly prohibited.
+'''
+
 import argparse
 import asyncio
 import logging
--- control_operator/frontend/lib/3d/scene_widget.dart
+++ control_operator/frontend/lib/3d/scene_widget.dart
@@ -1,3 +1,29 @@
+/**********************************************************************************
+ * Copyright (c) 2026 by Open Vision Technology, LLC., Massachusetts.
+ * All rights reserved. This material contains unpublished,
+ * copyrighted work, which includes confidential and proprietary
+ * information of Open Vision Technology, LLC..
+
+ * Open Vision Technology, LLC. and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto. Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from Open Vision Technology, LLC. is strictly prohibited.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **********************************************************************************
+ */
+
 import 'package:flutter/material.dart';
 
 import 'scene_widget_native.dart' if (dart.library.js) 'scene_widget_web.dart';
